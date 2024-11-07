c1a409662b1495e36f2705de6479a361500d19a7
Marcin Kowalski <marcinmac@MacBook-Pro-Marcin.local>
2024-11-07 16:48:30 +0100

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			external: ['tailwind-merge']
+		}
+	},
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
