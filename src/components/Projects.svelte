aa4cece2e27fdb356263de006fa904f11071a865
Marcin Kowalski <marcinmac@MacBook-Pro-Marcin.local>
2024-11-07 12:16:30 +0100

    second one

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -9,7 +9,7 @@
 <div class=" h-full sm:h-screen bg-blue text-white">
 	<Header text="technologies" centered capitalized textSize={size.xl} />
 	<div class="flex flex-col sm:flex-row justify-center">
-		<Card icon={reactIcon} text={{ eng: 'react', pl: 'react pl' }} header={'react!'} />
+		<Card icon={reactIcon} text={{ eng: 'react', pl: 'react PL' }} header={'react!'} />
 		<Card icon={tailwind} text={'tailwind'} header={'taiwind'} />
 		<Card icon={reactIcon} text={'react'} header={'react!'} />
 		<Card icon={tailwind} text={'tailwind'} header={'taiwind'} />
